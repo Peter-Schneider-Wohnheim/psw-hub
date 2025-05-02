183a7757afd6e4ad5cc0c8025583bae5c1569ab7
Timo Reusch <git@timo-reusch.de>
2025-05-02 18:07:24 +0200

    Provide description for more clarity

--- keychain/index.php
+++ keychain/index.php
@@ -2,5 +2,6 @@
 include(dirname(__DIR__). '/includes/header.inc.php');
 require_once(dirname(__DIR__). '/includes/components.php');
 echo('<h2 class="h2 text-center"><i class="bi bi-key"></i> | Keychain</h2>');
+echo('<div class="alert alert-primary" role="alert">This application allows you to unlock rooms with your PSW-Account.</div>');
 tile("/keychain/kitchen.php", "Kitchen", "bi-cup-straw");
 include(dirname(__DIR__). '/includes/footer.inc.php');
