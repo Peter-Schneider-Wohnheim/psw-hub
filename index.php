f22311f1099dbebb8a322b389ee7ad54ae10350b
Timo Reusch <git@timo-reusch.de>
2025-05-02 18:07:57 +0200

    Improvements (#2)
    * Make instructions clearer
    
    * Display customer number
    
    * Improve dashboard
    
    * Provide description for more clarity

--- index.php
+++ index.php
@@ -3,15 +3,23 @@ include(__DIR__ . '/includes/header.inc.php');
 require_once(__DIR__ . '/includes/components.php');
 
 echo('<h2 class="h2 mb-4">Welcome, ' . $_SESSION['displayname'] . '!</h2>');
-echo('<h3 class="h3 mb-4 mt-3">Tools</h3>');
-echo('<div class="tiles-container">');
+
 if (in_array("tutor", $_SESSION['roles']) or in_array("administrator", $_SESSION['roles'])){
+    echo('<h3 class="h3 mb-4 mt-3">Tutor</h3>');
+    echo('<div class="tiles-container">');
+    tile("https://cloud.psw-wuerzburg.de/index.php/f/3457", "Files, Mails & more", "bi-file-earmark-richtext");
     tile("/tutor/tenants", "Tenant List", "bi-person-lines-fill");
+    tile("https://psw-wuerzburg.de/wp-admin/", "WordPress Backend", "bi-person-lines-fill");
+    tile("https://booking.psw-wuerzburg.de/report.php", "Room Bookings", "bi-calendar-week");
+    echo('</div>');
 }
+echo('<h3 class="h3 mb-4 mt-3">Tenant</h3>');
+echo('<div class="tiles-container">');
 tile("https://psw-wuerzburg.de/wiki/", "Wiki", "bi-wikipedia");
-tile("/calendar", "Event Calendar", "bi-calendar-event");
 tile("https://booking.psw-wuerzburg.de/", "Room Booking", "bi-journal-bookmark-fill");
 tile("/keychain", "Keychain", "bi-key");
+tile("/calendar", "Event Calendar", "bi-calendar-event");
+tile("/vodafone", "Vodafone Customer ID", "bi-router");
 tile("/auction", "Bike Auction", "bi-bicycle");
 tile("https://cloud.psw-wuerzburg.de", "Cloud", "bi-cloud");
 
